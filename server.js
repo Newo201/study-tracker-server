96a93d3665f881632de5d1ff4faf92052165eec9
newo200 <newo200coding@gmail.com>
2024-06-05 21:48:46 +1000

    Connect database and set up environment variables

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from "express"
 import moment from "moment"
+import env from "dotenv";
 
 const port = 3000
 const app = express()
+
+env.config()
+
+const db = ({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+})
+
+db.connect()
 
 const test = moment("2024-06-04", "YYYY-MM-DD").week()
 
