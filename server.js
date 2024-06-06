61e6839228df010d6384b766db7e9b981dcceb15
newo200 <newo200coding@gmail.com>
2024-06-06 19:33:13 +1000

    Get request for study

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express"
+import bodyParser from "body-parser";
+import pg from "pg"
 import moment from "moment"
 import env from "dotenv";
 
-const port = 3000
+const port = 5000
 const app = express()
 
 env.config()
 
-const db = ({
+const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
@@ -17,11 +19,22 @@ const db = ({
 
 db.connect()
 
+// Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const test = moment("2024-06-04", "YYYY-MM-DD").week()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post("/study", async (req, res) => {
+    // Add in Date Filtering
+    const result = await db.query("SELECT week_completed, SUM(study_unit) study_completed FROM study GROUP BY week_completed ORDER BY week_completed")  
+    console.log(result.rows)
+    res.send(result.rows)
+}) 
 
 console.log(test)
 
