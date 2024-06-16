ed262d8ba3586c6d6492f8837200e7179115e22a
newo200 <newo200coding@gmail.com>
2024-06-16 20:49:08 +1000

    Change all post requests to get requests and add parameters

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.connect()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const test = moment("2024-06-04", "YYYY-MM-DD").week()
+// const test = moment("2024-06-04", "YYYY-MM-DD").week()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -33,38 +33,45 @@ app.get('/', (req, res) => {
 app.get("/study", async (req, res) => {
     // Add in Date Filtering
     const {start_date, end_date} = req.query
-    console.log(req.query)
     const result = await db.query("SELECT week_completed, SUM(study_unit) study_completed FROM study WHERE completed BETWEEN $1 AND $2 GROUP BY week_completed ORDER BY week_completed",
     [start_date, end_date])  
     console.log(result.rows)
     res.json(result.rows)
 }) 
 
-app.post("/study/subject", async (req, res) => {
+app.get("/study/subject", async (req, res) => {
     // For stacked bar chart we want to group by the week completed as well
+    const {start_date, end_date} = req.query
     let result
-    if (req.body.filterWeek) {
-        result = await db.query("SELECT subject, week_completed, SUM(study_unit) study_completed FROM study GROUP BY subject, week_completed ORDER BY subject") 
+    if (req.query.filterWeek) {
+        result = await db.query("SELECT subject, week_completed, SUM(study_unit) study_completed FROM study WHERE completed BETWEEN $1 AND $2 GROUP BY subject, week_completed ORDER BY subject",
+            [start_date, end_date]
+        ) 
     } else {
-        result = await db.query("SELECT subject, SUM(study_unit) study_completed FROM study GROUP BY subject ORDER BY subject") 
+        result = await db.query("SELECT subject, SUM(study_unit) study_completed FROM study WHERE completed BETWEEN $1 AND $2 GROUP BY subject ORDER BY subject",
+            [start_date, end_date]
+        ) 
     }
     console.log(result.rows)
     res.send(result.rows)
 })
 
-app.post("/study/type", async (req, res) => {
+app.get("/study/type", async (req, res) => {
     // For stacked bar chart we want to group by the week completed as well
+    const {start_date, end_date} = req.query
     let result
-    if (req.body.filterWeek) {
-        result = await db.query("SELECT study_type, week_completed, SUM(study_unit) study_completed FROM study GROUP BY study_type, week_completed ORDER BY study_type")
+    if (req.query.filterWeek) {
+        result = await db.query("SELECT study_type, week_completed, SUM(study_unit) study_completed FROM study WHERE completed BETWEEN $1 AND $2 GROUP BY study_type, week_completed ORDER BY study_type",
+            [start_date, end_date]
+        )
     } else {
-        result = await db.query("SELECT study_type, SUM(study_unit) study_completed FROM study GROUP BY study_type ORDER BY study_type")  
+        result = await db.query("SELECT study_type, SUM(study_unit) study_completed FROM study WHERE completed BETWEEN $1 AND $2 GROUP BY study_type ORDER BY study_type",
+            [start_date, end_date]
+        )  
     }
     console.log(result.rows)
     res.send(result.rows)
 })
-
-console.log(test)
 
 app.listen(port, () => {
     console.log(`App is litening on port ${port}`)
